a30158381df02eb8e1472a07b05e37bac78f49ba
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-09-24 16:04:06 -0300

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,10 +40,9 @@ namespace :deploy do
 
   task :setup_config do
     on roles(:app) do
-      sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-      sudo "ln -nfs #{current_path}/config/unicorn_ini.sh /etc/init.d/unicorn_#{application}"
-      run "mkdir -p #{shared_path}/config"
-      put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
+      sudo "ln -nfs #home/deploy/apps/foreman4rails/current/config/nginx.conf /etc/nginx/sites-enabled/foreman4rails"
+      sudo "ln -nfs #home/deploy/apps/foreman4rails/current/config/unicorn_ini.sh /etc/init.d/unicorn_foreman4rails"
+      put File.read("config/database.yml"), "#home/deploy/apps/foreman4rails/shared/config/database.yml"
       puts "Now edit the config files in #{shared_path}."
     end
   end
@@ -51,7 +50,7 @@ namespace :deploy do
 
   task :symlink_config do 
   	on roles(:app) do
-      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+      run "ln -nfs #home/deploy/apps/foreman4rails/shared/config/database.yml #{release_path}/config/database.yml"
     end
   end
   after "deploy:setup_config", "deploy:symlink_config"
