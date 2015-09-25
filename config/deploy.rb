f9bf229fe9902f6ea00a005be73eebe1664cd11c
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-09-25 14:03:19 -0300

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,9 +51,9 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:web) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      unless 'git rev-parse HEAD' == 'git rev-parse origin/master'
         puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
+        puts "Run 'git push' to sync changes."
         exit
       end
     end  
