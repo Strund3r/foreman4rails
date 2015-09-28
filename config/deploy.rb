7805d7d249593f01d2c7aebfce61f4c35ac19962
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-09-28 14:37:55 -0300

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,8 +67,8 @@ namespace :foreman do
   desc "Export the Procfile to Ubuntu's upstart scripts"
   task :export do
     on roles(:app) do
-      within "/home/deploy/apps/foreman4rails/current" do
-        execute "foreman export upstart /etc/init --procfile=/home/deploy/apps/foreman4rails/current/Procfile -a #{fetch(:application)} -u #{fetch(:user)} -l #{current_path}/log"
+      within current_path do
+        execute "cd #{current_path} && foreman export upstart /etc/init --procfile=/home/deploy/apps/foreman4rails/current/Procfile -a #{fetch(:application)} -u #{fetch(:user)} -l #{current_path}/log"
       end
     end
 
