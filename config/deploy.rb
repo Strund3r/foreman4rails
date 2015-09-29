0676e885eba3101824d813de7ea94c842cafc5aa
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-09-29 14:20:05 -0300

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,8 +68,7 @@ namespace :foreman do
   task :export do
     on roles(:app) do
         execute "sudo chmod -R 1777 /etc/init/"
-        foreman_export = "foreman export upstart /etc/init --app=#{fetch(:application)} --user=#{fetch(:user)}"
-        execute foreman_export
+        execute "#{sudo} foreman export upstart /etc/init --app=#{fetch(:application)} --user=#{fetch(:user)}"        
     end
   end
 
