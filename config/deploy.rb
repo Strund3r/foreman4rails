145a4697916a6586221fabee41ed696248c3abe3
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-09-30 15:03:02 -0300

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -71,6 +71,10 @@ namespace :app do
   task :update_rvm_key do
     execute :gpg, "--keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
   end
+  desc "install rvm"
+  task :install do
+    execute "rvm install 2.2.3"
+  end
 end
 before "rvm1:install:rvm", "app:update_rvm_key"
 
