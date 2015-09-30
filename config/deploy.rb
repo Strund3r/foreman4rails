b526b40e61e9a9ce597ad57dea0d8075d0637466
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-09-30 13:31:46 -0300

    Updated deploy.rb IP

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.4.0'
 require "capistrano/bundler"
 
 # Define your server here
-server "159.203.78.204", roles: %w{web app db}, primary: true
+server "45.55.87.168", roles: %w{web app db}, primary: true
 
 
 # Set application settings
