c0d3457be4e520ca8037291a3df2901e2a11edfa
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-09-30 15:42:00 -0300

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -79,10 +79,10 @@ end
 before "rvm1:install:rvm", "app:update_rvm_key"
 after "app:update_rvm_key", "app:install_rvm"
 
-before 'deploy:check_revision', 'rvm1:install:rvm'   # install/update RVM
-before 'deploy:check_revision', 'rvm1:install:ruby'  # install/update Ruby
-before 'deploy:check_revision', 'rvm1:alias:create'
-before 'deploy:check_revision', 'rvm1:install:gems'  # install/update gems from Gemfile into gemset
+before 'deploy', 'rvm1:install:rvm'   # install/update RVM
+before 'deploy', 'rvm1:install:ruby'  # install/update Ruby
+before 'deploy', 'rvm1:alias:create'
+before 'deploy', 'rvm1:install:gems'  # install/update gems from Gemfile into gemset
 
 namespace :foreman do
   desc "Export the Procfile to Ubuntu's upstart scripts"
