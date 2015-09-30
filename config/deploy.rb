50fba01b38b365a6c90051988f84788eba43018c
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-09-30 15:56:37 -0300

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,23 +67,6 @@ namespace :deploy do
   before "deploy", "deploy:check_revision"
 end
 
-namespace :app do
-  task :update_rvm_key do
-    execute :gpg, "--keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
-  end
-
-  task :install_rvm do
-    execute "rvm install 2.2.3"
-  end
-end
-before "rvm1:install:rvm", "app:update_rvm_key"
-after "app:update_rvm_key", "app:install_rvm"
-
-before 'deploy', 'rvm1:install:rvm'   # install/update RVM
-before 'deploy', 'rvm1:install:ruby'  # install/update Ruby
-before 'deploy', 'rvm1:alias:create'
-before 'deploy', 'rvm1:install:gems'  # install/update gems from Gemfile into gemset
-
 namespace :foreman do
   desc "Export the Procfile to Ubuntu's upstart scripts"
   task :export do
