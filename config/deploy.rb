b4db9f28617dbb4c33a675104a0242eea97d6dc4
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-10-01 15:54:25 -0300

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -102,8 +102,8 @@ namespace :foreman do
   desc "Restart the application services"
   task :restart do
     on roles(:app) do
-#        execute "sudo stop #{fetch(:application)}"
-        execute "sudo start #{fetch(:application)} || sudo restart #{fetch(:application)}"
+        execute "sudo stop #{fetch(:application)}"
+#        execute "sudo start #{fetch(:application)} || sudo restart #{fetch(:application)}"
     end
   end
 end
