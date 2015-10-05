08fd7d9930bad69a2cda68f4836a99f56e55a503
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-10-05 15:44:57 -0300

    Updated StepByStep

--- StepByStep.rb
+++ StepByStep.rb
@@ -498,15 +498,16 @@ Adicionar usuário ao servidor: sudo adduser deploy
 
 
 Instalar Ruby 2.2.3: sudo apt-get update
-		     sudo apt-get install libgmp3-dev
-		     sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+		     sudo apt-get install -y libgmp3-dev
+		     sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
 
 
 
 
-Instalar RVM: sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
-	      curl -L https://get.rvm.io | bash -s stable
+Instalar RVM: sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+	      gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+              curl -L https://get.rvm.io | bash -s stable
 	      source ~/.rvm/scripts/rvm
 	      rvm install 2.2.3
               rvm use 2.2.3 --default
@@ -517,15 +518,15 @@ Instalar RVM: sudo apt-get install libgdbm-dev libncurses5-dev automake libtool 
 
 
 Instalar Bundler gem: echo "gem: --no-ri --no-rdoc" > ~/.gemrc
-		 gem install bundler
+		      gem install bundler
 
 
 
 
 
 Instalar NodeJS: sudo add-apt-repository ppa:chris-lea/node.js
-		 sudo apt-get update
-		 sudo apt-get install nodejs
+		 sudo apt-get -y update
+		 sudo apt-get -y install nodejs
 
 
 
