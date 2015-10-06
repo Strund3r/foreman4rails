edec4119e1773f433527466781431ec1b625fd1d
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-10-06 12:28:28 -0300

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,8 +56,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:web) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/#{fetch(:branch)`
-        puts "WARNING: HEAD is not the same as origin/#{fetch(:branch)"
+      unless `git rev-parse HEAD` == `git rev-parse origin/automatizar-nginx-unicorn`
+        puts "WARNING: HEAD is not the same as origin/automatizar-nginx-unicorn"
         puts "Run `git push` to sync changes."
         exit
       end
