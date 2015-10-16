79a0742646e7c2680ec995d2c0cdbae8c3da4d22
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-10-16 18:29:25 -0300

    Updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :forward_agent, true
 #set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-#set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 
 
