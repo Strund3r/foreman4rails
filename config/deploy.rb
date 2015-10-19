ecaea74243c474f392c033f5bcdb1a8ac8c08901
dev9seucondominio <dev9.seucondominio@gmail.com>
2015-10-19 13:20:02 -0200

    Removed cat app's log

--- config/deploy.rb
+++ config/deploy.rb
@@ -96,7 +96,6 @@ namespace :foreman do
   task :restart do
     on roles(:app) do
       execute "sudo start #{fetch(:application)} || sudo restart #{fetch(:application)}"
-      execute "sudo cat /var/log/upstart/foreman4rails-web-1.log"
     end
   end
 end
